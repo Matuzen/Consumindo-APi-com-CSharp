c31c17cbcdc73b2d4daaa7fc0030df5cdda5715e
Matheus Alexandre <104906223+Matuzen@users.noreply.github.com>
2024-01-04 10:37:26 -0300

    Retornando todas as músicas
    Aqui eu criei uma classe Musica.cs com apenas 4 propriedades para manipular o JSON (serialização e desserialização com c#) retornando todas as músicas e também retornando uma música específica em um determinado índice

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using ScreenSound_04.Models;
+using System.Text.Json;
+
+using (HttpClient client = new())
+{
+    try
+    {
+        var resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        Console.WriteLine(resposta);
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        musicas[19989].ExibirDetalhesDaMusica();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Algo deu errado:" + ex.Message);
+    }
+}
